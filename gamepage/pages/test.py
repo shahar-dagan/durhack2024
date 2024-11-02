6278c7c846c37ff354726893c68d6649d27a4703
bonds303 <samtbond2005@gmail.com>
2024-11-02 23:06:20 +0000

    improved skeleton layout

--- gamepage/pages/test.py
+++ gamepage/pages/test.py
@@ -5,8 +5,9 @@ st.set_page_config(page_title="Skeleton Layout Example", layout="wide")
 
 # Header section
 st.title("TITLE")
-st.subheader("lalalalalalalala okkokokokokok")
-
 st.image("images/Chur Bum.jpg")
+st.write("lalalalalalalala okkokokokokok")
+
+
 if st.button("NEXT PAGE"):
     page =+ 1
